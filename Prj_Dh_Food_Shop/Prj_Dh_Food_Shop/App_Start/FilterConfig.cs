cb4b48d0c0fff70a9400235c3ac280630357f677
Sơn Shadow <64057133+sonVT1999@users.noreply.github.com>
2022-01-01 00:10:56 +0700

    tạo project

---
+++ Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Prj_Dh_Food_Shop
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prj_Dh_Food_Shop.MvcApplication" Language="C#" %>
