0a0a6912befd063ef51ff92568578837fe2cd115
Sơn Shadow <64057133+sonVT1999@users.noreply.github.com>
2022-01-04 17:49:14 +0700

    update template layout

---
+++ Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Controllers/DashboardsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prj_Dh_Food_Shop.Controllers
+{
+    public class DashboardsController : Controller
+    {
+        // GET: Dashboards
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prj_Dh_Food_Shop.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
