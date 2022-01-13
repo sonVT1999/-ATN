3beada4753434168444232b61fcdc374ab644f9b
Sơn Shadow <64057133+sonVT1999@users.noreply.github.com>
2022-01-13 14:20:21 +0700

    update code chức năng tìm kiếm, phân trang Users

--- Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Controllers/UsersController.cs
+++ Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Controllers/UsersController.cs
@@ -16,11 +16,40 @@ namespace Prj_Dh_Food_Shop.Controllers
         private Entity_Dh_Food db = new Entity_Dh_Food();
 
         // GET: Users
-        public ActionResult Index()
+        public ActionResult Index(Search_Users model)
         {
-            var NguoiDung = db.Users.Where(x => x.id != 0);
+            model.txbName = model.txbName == null ? string.Empty : model.txbName.Trim();
+            model.txbUsername = model.txbUsername == null ? string.Empty : model.txbUsername.Trim();
+            model.txbPhoneNumber = model.txbPhoneNumber == null ? string.Empty : model.txbPhoneNumber.Trim();
+
+            model.page = model.page == 0 ? 1 : model.page;
+            model.pageSize = model.pageSize == 0 ? 2 : model.pageSize;
+
+            var data = from c in db.Users
+                       join p in db.Districts on c.id_district equals p.id
+                       where (string.IsNullOrEmpty(model.txbName) || c.name.Contains(model.txbName))
+                       && (!string.IsNullOrEmpty(model.username) || c.username.Contains(model.txbUsername)) && (!string.IsNullOrEmpty(model.phone_number) || c.phone_number.Contains(model.txbPhoneNumber))
+                       select new Search_Users()
+                       {
+                           id = c.id,
+                           name = c.name,
+                           username = c.username,
+                           passwords = c.passwords,
+                           phone_number = c.phone_number,
+                           addresss = c.addresss,
+                           is_active = c.is_active,
+                           permission = c.permission,
+                           district_name = p.name,
+                       };
+
+            var rs = data.OrderBy(x => x.id).Skip(((model.page - 1) * model.pageSize)).Take(model.pageSize).ToList() ?? new List<Search_Users>();
+
             ViewBag.district = new UsersController().getDistricts();
-            return View(db.Users.OrderByDescending(x => x.id));
+
+            model.lstData = rs;
+            model.totalRecord = data.Count();
+            model.totalPage = (int)Math.Ceiling((decimal)model.totalRecord / model.pageSize);
+            return View(model);
         }
 
         [HttpPost]
@@ -156,6 +185,66 @@ namespace Prj_Dh_Food_Shop.Controllers
 
             return Json(new { msg = msgDel, status = status }, JsonRequestBehavior.AllowGet);
         }
+
+        //[HttpPost]
+        //public ActionResult Search(Search_Users model)
+        //{
+        //    if (model.txbName != "" || model.txbUsername != "" || model.txbPhoneNumber != "")
+        //    {
+        //        model.txbName = model.txbName.Trim();
+        //        model.txbUsername = model.txbUsername.Trim();
+        //        model.txbPhoneNumber = model.txbPhoneNumber.Trim();
+        //    }
+        //    model.page = model.page == 0 ? 1 : model.page;
+        //    model.pageSize = model.pageSize == 0 ? 5 : model.pageSize;
+
+        //    var data = from c in db.CongNhan
+        //               join p2 in db.Phuong on c.id_phuong equals p2.id
+        //               where (string.IsNullOrEmpty(model.searchString) || c.ten.Contains(model.searchString))
+        //               && (!model.id_phong_ban.HasValue || c.id_phong_ban == model.id_phong_ban) && (!model.id_phuong.HasValue || c.id_phuong == model.id_phuong)
+        //               select new Search_Users()
+        //               {
+        //                   id = c.id,
+        //                   id_phuong = c.id_phuong,
+        //                   id_phong_ban = c.id_phong_ban,
+        //                   que_quan = c.que_quan,
+        //                   phuong_name = p2.ten,
+        //                   phong_name = p.ten,
+        //                   ten = c.ten,
+        //               };
+
+        //    var rs = data.OrderBy(x => x.id).Skip(((model.page - 1) * model.pageSize)).Take(model.pageSize).ToList() ?? new List<Search_Users>();
+
+        //    var lstDistrict = new UsersController().getDistricts();
+
+        //    ViewBag.phuong = lstDistrict;
+        //    //model.lstDistrict = lstDistrict.ConvertAll(a => new SelectListItem()
+        //    //{
+        //    //    Text = a.name,
+        //    //    Value = a.id.ToString(),
+        //    //}) ?? new List<SelectListItem>();
+
+        //    model.lstData = rs;
+        //    model.totalRecord = data.Count();
+        //    model.totalPage = (int)Math.Ceiling((decimal)model.totalRecord / model.pageSize);
+        //    return View(model);
+        //}
+
+
+        //{
+        //    List<Users> usser = new List<Users>();
+        //    if (txbName != "" || txbUsername != "" || txbPhoneNumber != "")
+        //    {
+        //        usser = db.Users.SqlQuery("select * from Users where Users.name like N'%" + txbName + "%' and Users.username like N'%" + txbUsername + "%' and Users.phone_number like N'%" + txbPhoneNumber + "%'").ToList();
+        //        ViewBag.district = new UsersController().getDistricts();
+        //    }
+        //    else
+        //    {
+        //        usser = db.Users.Where(x => x.id != 0).ToList();
+        //        ViewBag.district = new UsersController().getDistricts();
+        //    }
+        //    return View("Index", usser.OrderByDescending(x => x.id));
+        //}
 
     }
 }
---
+++ Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Models_add/Search_Users.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prj_Dh_Food_Shop
+{
+    public class Search_Users
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string username { get; set; }
+
+        public string passwords { get; set; }
+
+        public string phone_number { get; set; }
+
+        public string addresss { get; set; }
+
+        public DateTime create_date { get; set; }
+
+        public int is_active { get; set; }
+
+        public string permission { get; set; }
+
+        public int id_district { get; set; }
+
+        public string district_name { get; set; }
+
+        public virtual Districts Districts { get; set; }
+
+        public List<Search_Users> lstData { get; set; }
+
+        public string txbName { get; set; }
+
+        public string txbUsername { get; set; }
+
+        public string txbPhoneNumber { get; set; }
+
+        public int page { get; set; }
+
+        public int pageSize { get; set; }
+
+        public int totalRecord { get; set; }
+
+        public int totalPage { get; set; }
+    }
+}
