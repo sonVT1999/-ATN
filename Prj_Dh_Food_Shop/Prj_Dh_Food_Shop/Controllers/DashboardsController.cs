c60f125806820dd4257207eb2c75042fc7d4b8f5
Sơn Shadow <64057133+sonVT1999@users.noreply.github.com>
2022-03-06 23:56:25 +0700

    update code bảng Dashboard

--- Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Controllers/DashboardsController.cs
+++ Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Controllers/DashboardsController.cs
@@ -16,7 +16,27 @@ namespace Prj_Dh_Food_Shop.Controllers
             ViewData["soluongKH"] = db.Customers.Where(x => x.id != 0).Count();
             ViewData["soluongKHTN"] = db.Customers_potential.Where(x => x.statuss != "Đã gọi").Count();
             ViewData["soluongFeedback"] = db.Feedbacks.Where(x => x.is_active == 2).Count();
+            ViewBag.Inner = new DashboardsController().SelectTop5Product();
             return View();
+        }
+
+        public List<SearchTop_Products> SelectTop5Product()
+        {
+            var rs = db.Database.SqlQuery<SearchTop_Products>("select top 5 Products.id, Products.name as TenSP, Categories.name as category_name, SUM(Orders_detail.amount) as doanhthu "
+                                                              + "from Products, Orders, Orders_detail, Categories "
+                                                              + "where Products.id_category = Categories.id "
+                                                              + "and Products.id = Orders_detail.id_product "
+                                                              + "and Orders_detail.id_order = Orders.id "
+                                                              + "and Orders.statuss = 2 "
+                                                              + "GROUP BY Products.id, Products.name, Categories.name "
+                                                              + "ORDER BY doanhthu DESC").ToList();
+            return rs;
+        }
+
+        public List<Categories> Loai()
+        {
+            var model = db.Categories.ToList();
+            return model;
         }
     }
 }
---
+++ Prj_Dh_Food_Shop/Prj_Dh_Food_Shop/Models_add/SearchTop_Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prj_Dh_Food_Shop
+{
+    public class SearchTop_Products
+    {
+        public int id { get; set; }
+
+        public string tenSP { get; set; }
+
+        public string category_name { get; set; }
+
+        public virtual Categories categories { get; set; }
+
+        public double? doanhthu { get; set; }
+    }
+}
